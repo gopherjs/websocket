545ce6940a04a053bf438f4fac0460b252147c49
Michael Johnson <michael@nightexcessive.us>
2014-11-08 20:29:13 -0500

    Update examples

--- example/client/client.go
+++ example/client/client.go
@@ -3,35 +3,91 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 
-	"github.com/gopherjs/gopherjs/js"
 	"honnef.co/go/js/console"
 	"honnef.co/go/js/dom"
 
 	"github.com/gopherjs/websocket"
 )
 
-func testEcho(socket *websocket.WebSocket, message []byte) {
-	console.Time(fmt.Sprintf("WebSocket: Echo (binary): % x", message))
-	if _, err := socket.Write(message); err != nil {
+func testEchoBytes(c *websocket.Conn, message []byte) {
+	fmt.Printf("testEchoBytes: % x\n", message)
+	if _, err := c.Write(message); err != nil {
 		panic(fmt.Sprintf("Error when sending: %s\n", err))
-		return
 	}
 
-	incomingMessage := make([]byte, len(message))
-	n, errorEvent := socket.Read(incomingMessage)
+	incomingMessage := make([]byte, len(message)+1)
+
+	fmt.Println("Read one...")
+	n, errorEvent := c.Read(incomingMessage[:len(incomingMessage)-2])
 	if errorEvent != nil {
 		panic(fmt.Sprintf("Error when receiving: %s\n", errorEvent))
-		return
 	}
-	receivedBytes := incomingMessage[:n]
 
-	if bytes.Compare(receivedBytes, message) != 0 {
-		console.TimeEnd(fmt.Sprintf("WebSocket: Echo (binary): % x", message))
+	fmt.Println("Read two...")
+	n2, errorEvent := c.Read(incomingMessage[len(incomingMessage)-2:])
+	if errorEvent != nil {
+		panic(fmt.Sprintf("Error when receiving: %s\n", errorEvent))
+	}
+	n += n2
+
+	fmt.Printf("Results: % x (%d)\n", incomingMessage, n)
+
+	receivedBytes := incomingMessage[:n]
+	fmt.Printf("Finalized: % x\n", receivedBytes)
+
+	if !bytes.Equal(receivedBytes, message) {
 		console.Warn(fmt.Sprintf("Received unexecpected message: % x (expected % x)", receivedBytes, message))
 		return
 	}
-	console.TimeEnd(fmt.Sprintf("WebSocket: Echo (binary): % x", message))
+
+	fmt.Println("Done with testEchoBytes.")
+}
+
+func testEchoString(c *websocket.Conn, message string) {
+	fmt.Printf("testEchoString: %q\n", message)
+	if _, err := io.WriteString(c, message); err != nil {
+		panic(fmt.Sprintf("Error when sending: %s\n", err))
+	}
+
+	incomingMessage := make([]byte, len(message)+1)
+
+	fmt.Println("Read one...")
+	n, errorEvent := c.Read(incomingMessage[:len(incomingMessage)-2])
+	if errorEvent != nil {
+		panic(fmt.Sprintf("Error when receiving: %s\n", errorEvent))
+	}
+
+	fmt.Println("Read two...")
+	n2, errorEvent := c.Read(incomingMessage[len(incomingMessage)-2:])
+	if errorEvent != nil {
+		panic(fmt.Sprintf("Error when receiving: %s\n", errorEvent))
+	}
+	n += n2
+
+	fmt.Printf("Results: % x (%d)\n", incomingMessage, n)
+
+	receivedBytes := incomingMessage[:n]
+	fmt.Printf("Finalized: % x\n", receivedBytes)
+
+	receivedString := string(receivedBytes)
+	fmt.Printf("Stringified: %s\n", receivedString)
+
+	if receivedString != message {
+		console.Warn(fmt.Sprintf("Received unexecpected message: %q (expected %q)", receivedBytes, message))
+		return
+	}
+
+	fmt.Println("Done with testEchoString.")
+}
+
+func stepOne(c *websocket.Conn) {
+	testEchoString(c, "Hello, String!")
+}
+
+func stepTwo(c *websocket.Conn) {
+	testEchoBytes(c, []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05})
 }
 
 func main() {
@@ -44,22 +100,23 @@ func main() {
 			wsProtocol = "wss"
 		}
 
-		console.Time("WebSocket: Connect")
-		socket, err := websocket.New(fmt.Sprintf("%s://%s:%s/echo", wsProtocol, location.Hostname, location.Port))
+		console.Time("Dial")
+		c, err := websocket.Dial(fmt.Sprintf("%s://%s:%s/echo", wsProtocol, location.Hostname, location.Port))
 		if err != nil {
 			fmt.Printf("Failed to connect: %s\n", err)
 			return
 		}
-		console.TimeEnd("WebSocket: Connect")
+		console.TimeEnd("Dial")
 
-		/*defer func() {
-			socket.Close()
-			console.Log("WebSocket: Disconnected")
-		}()*/
+		defer func() {
+			if err := c.Close(); err != nil {
+				console.Warn("Error while disconnecting:", err)
+				return
+			}
+			fmt.Printf("Cleanly disconnected.")
+		}()
 
-		testEcho(socket, []byte("Hello, World!"))
-		testEcho(socket, []byte("World, Hello!"))
-		testEcho(socket, []byte{0x01, 0x02, 0x03})
-		js.Global.Set("testSocket", socket)
+		stepOne(c)
+		stepTwo(c)
 	}()
 }
--- example/server/server.go
+++ example/server/server.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"io"
+	"bytes"
+	"fmt"
 	"net/http"
 	"os"
 
@@ -9,7 +10,28 @@ import (
 )
 
 func echoHandler(ws *websocket.Conn) {
-	io.Copy(ws, ws)
+	var strMessage string
+	fmt.Println("Receiving first message...")
+	websocket.Message.Receive(ws, &strMessage)
+	fmt.Printf("Got first message: %q\n", strMessage)
+	if strMessage != "Hello, String!" {
+		fmt.Println("First message was bad.")
+		return
+	}
+	fmt.Println("Returning first message...")
+	websocket.Message.Send(ws, strMessage)
+
+	var byteMessage []byte
+	fmt.Println("Receiving second message...")
+	websocket.Message.Receive(ws, &byteMessage)
+	fmt.Printf("Got second message: % x\n", byteMessage)
+	if !bytes.Equal(byteMessage, []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05}) {
+		fmt.Println("Second message was bad.")
+		return
+	}
+	fmt.Println("Returning second message...")
+	websocket.Message.Send(ws, byteMessage)
+	fmt.Println("Done.")
 }
 
 func main() {
