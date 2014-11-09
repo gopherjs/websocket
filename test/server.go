f03f42543d2464c5553b7a2a420a55722698f8a0
Michael Johnson <michael@nightexcessive.us>
2014-11-09 15:33:02 -0500

    Initial tests

---
+++ test/server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"go/build"
+
+	"code.google.com/p/go.net/websocket"
+	"github.com/codegangsta/martini"
+)
+
+func main() {
+	m := martini.Classic()
+
+	//serve test folder
+	m.Use(martini.Static("test"))
+
+	//serve sourcemaps from GOROOT and GOPATH
+	m.Use(martini.Static(build.Default.GOROOT, martini.StaticOptions{Prefix: "goroot"}))
+	m.Use(martini.Static(build.Default.GOPATH, martini.StaticOptions{Prefix: "gopath"}))
+
+	m.Get("/ws/immediate-close", websocket.Handler(func(ws *websocket.Conn) {
+		// Cleanly close the connection.
+		if err := ws.Close(); err != nil {
+			panic(err)
+		}
+	}).ServeHTTP)
+
+	m.Get("/ws/binary-static", websocket.Handler(func(ws *websocket.Conn) {
+		if err := websocket.Message.Send(ws, []byte{0x00, 0x01, 0x02, 0x03, 0x04}); err != nil {
+			panic(err)
+		}
+	}).ServeHTTP)
+
+	m.Run()
+}
---
+++ test/test/index.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+
+	"github.com/gopherjs/gopherjs/js"
+	"github.com/gopherjs/websocket"
+	"github.com/rusco/qunit"
+	"honnef.co/go/js/dom"
+)
+
+func getWSBaseURL() string {
+	document := dom.GetWindow().Document().(dom.HTMLDocument)
+	location := document.Location()
+
+	wsProtocol := "ws"
+	if location.Protocol == "https:" {
+		wsProtocol = "wss"
+	}
+
+	return fmt.Sprintf("%s://%s:%s/ws/", wsProtocol, location.Hostname, location.Port)
+}
+
+func main() {
+	wsBaseURL := getWSBaseURL()
+
+	qunit.Module("websocket.WebSocket")
+	qunit.Test("Invalid URL", func(assert qunit.QUnitAssert) {
+		qunit.Expect(1)
+
+		ws, err := websocket.New("blah://blah.example/invalid")
+		if err == nil {
+			ws.Close()
+			assert.Ok(false, "Got no error, but expected an invalid URL error")
+			return
+		}
+
+		assert.Ok(true, fmt.Sprintf("Received an error: %s", err))
+	})
+	qunit.AsyncTest("Immediate close", func() interface{} {
+		qunit.Expect(2)
+
+		ws, err := websocket.New(wsBaseURL + "immediate-close")
+		if err != nil {
+			qunit.Ok(false, fmt.Sprintf("Error opening WebSocket: %s", err))
+			qunit.Start()
+			return nil
+		}
+
+		ws.AddEventListener("open", false, func(ev js.Object) {
+			qunit.Ok(true, "WebSocket opened")
+		})
+
+		ws.AddEventListener("close", false, func(ev js.Object) {
+			const CloseNormal = 1000
+			closeEvent := dom.WrapEvent(ev).(*dom.CloseEvent)
+			if closeEvent.Code != CloseNormal {
+				qunit.Ok(false, fmt.Sprintf("WebSocket close was not clean (code %d)", closeEvent.Code))
+				qunit.Start()
+				return
+			}
+			qunit.Ok(true, "WebSocket closed")
+			qunit.Start()
+		})
+
+		return nil
+	})
+
+	qunit.Module("websocket.Conn")
+	qunit.AsyncTest("Immediate close", func() interface{} {
+		go func() {
+			defer qunit.Start()
+
+			ws, err := websocket.Dial(wsBaseURL + "immediate-close")
+			if err != nil {
+				qunit.Ok(false, fmt.Sprintf("Error opening WebSocket: %s", err))
+				return
+			}
+
+			qunit.Ok(true, "WebSocket opened")
+
+			_, err = ws.Read(nil)
+			if err == io.EOF {
+				qunit.Ok(true, "Received EOF")
+			} else if err != nil {
+				qunit.Ok(false, fmt.Sprintf("Unexpected error in second read: %s", err))
+			} else {
+				qunit.Ok(false, "Expected EOF in second read, got no error")
+			}
+		}()
+
+		return nil
+	})
+	qunit.AsyncTest("Failed open", func() interface{} {
+		go func() {
+			defer qunit.Start()
+
+			ws, err := websocket.Dial(wsBaseURL + "404-not-found")
+			if err == nil {
+				ws.Close()
+				qunit.Ok(false, "Got no error, but expected an error in opening the WebSocket.")
+				return
+			}
+
+			qunit.Ok(true, fmt.Sprintf("WebSocket failed to open: %s", err))
+		}()
+
+		return nil
+	})
+	qunit.AsyncTest("Binary read", func() interface{} {
+		qunit.Expect(3)
+
+		go func() {
+			defer qunit.Start()
+
+			ws, err := websocket.Dial(wsBaseURL + "binary-static")
+			if err != nil {
+				qunit.Ok(false, fmt.Sprintf("Error opening WebSocket: %s", err))
+				return
+			}
+
+			qunit.Ok(true, "WebSocket opened")
+
+			var expectedData = []byte{0x00, 0x01, 0x02, 0x03, 0x04}
+
+			receivedData := make([]byte, len(expectedData))
+			n, err := ws.Read(receivedData)
+			if err != nil {
+				qunit.Ok(false, fmt.Sprintf("Error in first read: %s", err))
+				return
+			}
+			receivedData = receivedData[:n]
+
+			if !bytes.Equal(receivedData, expectedData) {
+				qunit.Ok(false, fmt.Sprintf("Received data did not match expected data. Got % x, expected % x.", receivedData, expectedData))
+			} else {
+				qunit.Ok(true, fmt.Sprintf("Received data: % x", receivedData))
+			}
+
+			_, err = ws.Read(receivedData)
+			if err == io.EOF {
+				qunit.Ok(true, "Received EOF")
+			} else if err != nil {
+				qunit.Ok(false, fmt.Sprintf("Unexpected error in second read: %s", err))
+			} else {
+				qunit.Ok(false, "Expected EOF in second read, got no error")
+			}
+		}()
+
+		return nil
+	})
+}
