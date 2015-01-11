d9ebf98b56aae8e7a9ec4d37d22756ea4721e42a
Michael Johnson <michael@nightexcessive.us>
2015-01-10 20:20:15 -0500

    Add license

--- addr.go
+++ addr.go
@@ -1,3 +1,7 @@
+// Copyright 2014-2015 GopherJS Team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
+
 package websocket
 
 import "net/url"
