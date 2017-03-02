f4d9d91d916df2d2c7a370174994f43b8f238d55
Nate Woods <big.nate.w@gmail.com>
2017-03-01 19:41:37 -0700

    Renaming core to websocketjs

--- websocketjs/websocketjs.go
+++ websocketjs/websocketjs.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be found
 // in the LICENSE file.
 
-package core
+package websocketjs
 
 import "github.com/gopherjs/gopherjs/js"
 
